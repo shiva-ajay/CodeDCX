8d844a8539078f1cd55f18ce20415226cd0ec2c7
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-09-10 17:18:51 +0530

    Handle Errors

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import {ErrorMiddleware} from "./middleware/error";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -32,3 +33,6 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
   err.statusCode = 404;
   next(err);
 });
+
+
+app.use(ErrorMiddleware);
