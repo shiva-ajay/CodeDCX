e9f9fc0d21946dae72c3e5860beb721c5df48205
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-09-18 01:37:01 +0530

    Edit API

--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
-import { createLayout } from "../controllers/layout.controller";
+import { createLayout, editLayout, getLayoutByType } from "../controllers/layout.controller";
 const layoutRouter = express.Router();
 
 layoutRouter.post("/create-layout", isAutheticated,authorizeRoles("admin"), createLayout);
 
+layoutRouter.put("/edit-layout", isAutheticated,authorizeRoles("admin"), editLayout);
 
+layoutRouter.get("/get-layout/:type",getLayoutByType);
 
 
 export default layoutRouter;
