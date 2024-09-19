141c1304247c19e9f2bd022814b88965041a7492
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-09-19 20:40:54 +0530

    Header, Hero Components

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 import React, { FC, useState } from "react";
-
+import Heading from "./utils/Heading";
+import Header from "./components/Header";
+import Hero from "./components/Route/Hero";
 
 interface Props {}
 
 const Page: FC<Props> = (props) => {
-
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
 
   return (
     <div>
+      <Heading
+        title="CodeDCX"
+        description="CodeDCX is a platform for students to learn and get help from teachers"
+        keywords="Prograaming,MERN,Redux,Machine Learning"
+      />
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
 
+      <Hero />
     </div>
   );
 };
